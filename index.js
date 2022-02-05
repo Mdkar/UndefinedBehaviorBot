0a4d5eddd775af7411b7bdca302d17062c0fc850
Mdkar <mihirmdkar@gmail.com>
2022-02-05 17:45:24 -0500

    trying to add commands

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
 
 client.login(process.env.BOT_TOKEN)
 
-client.on("ready", () => {
-    console.log("Bot is ready")
-})
-
 client.on("messageCreate", msg => {
     if (msg.content === "hey") {
         msg.reply("hi there")
@@ -17,3 +13,15 @@ client.on("messageCreate", msg => {
         msg.react("❤️")
     }
 })
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+  
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    if (interaction.commandName === 'ping') {
+        await interaction.reply('Pong!');
+    }
+});
