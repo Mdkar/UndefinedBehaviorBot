fe42b46238d79db95e3f4060cbb0c32139aa0f34
Rajeev Godse <rajgodse@gmail.com>
2022-02-05 22:38:00 -0500

    Moved secrets to .env

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,8 +1,11 @@
+require("dotenv").config()
 const fs = require("fs");
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
-const { clientId, guildId, token } = require("./config.json");
+const clientId = process.env.CLIENT_ID
+const guildId = process.env.GUILD_ID
+const token = process.BOT_TOKEN
 
 const commands = [
   new SlashCommandBuilder()
