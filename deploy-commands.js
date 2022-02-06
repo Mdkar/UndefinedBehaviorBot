62c5390b6cacbd4ceacfe4f296b9f3d889dcecf4
Ruth Lin <66189801+ruthielin@users.noreply.github.com>
2022-02-05 22:27:38 -0500

    got slash commands to work

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,17 +1,33 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId, token } = require('./config.json');
+const fs = require("fs");
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+const { clientId, guildId, token } = require("./config.json");
 
 const commands = [
-	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
-]
-	.map(command => command.toJSON());
+  new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with pong!"),
+  new SlashCommandBuilder()
+    .setName("sell")
+    .setDescription("Replies with sell a block!")
+    .addIntegerOption((option) =>
+      option
+        .setName("input")
+        .setDescription("The input to echo back")
+        .setRequired(true)
+    ),
+  new SlashCommandBuilder()
+    .setName("server")
+    .setDescription("Replies with server info!"),
+  new SlashCommandBuilder()
+    .setName("user")
+    .setDescription("Replies with user info!"),
+].map((command) => command.toJSON());
 
-const rest = new REST({ version: '9' }).setToken(token);
+const rest = new REST({ version: "9" }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error);
+rest
+  .put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+  .then(() => console.log("Successfully registered application commands."))
+  .catch(console.error);
--- index.js
+++ index.js
@@ -1,27 +1,31 @@
-require('dotenv').config()
-const Discord = require('discord.js')
-const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
+require("dotenv").config();
+const Discord = require("discord.js");
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
-client.login(process.env.BOT_TOKEN)
+client.login(process.env.BOT_TOKEN);
 
-client.on("messageCreate", msg => {
-    if (msg.content === "hey") {
-        msg.reply("hi there")
-    } else if (msg.content === "u good bro") {
-        msg.channel.send("nah")
-    } else if (msg.content === "rtn is epic") {
-        msg.react("❤️")
-    }
-})
-
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+client.on("messageCreate", (msg) => {
+  if (msg.content === "hey") {
+    msg.reply("hi there");
+  } else if (msg.content === "u good bro") {
+    msg.channel.send("nah");
+  } else if (msg.content === "rtn is epic") {
+    msg.react("❤️");
+  } else if (msg.content === "ruthie is the best") {
+    msg.channel.send("yes i agree");
+  }
 });
-  
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
 
-    if (interaction.commandName === 'ping') {
-        await interaction.reply('Pong!');
-    }
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === "ping") {
+    await interaction.reply("Pong!");
+  } else if (interaction.commandName === "sell") {
+    await interaction.reply("Sell a block!");
+  }
 });
